e7d5d46f5c98a42f75003768732939cebca97c89
kenzieadmin <kenzieadmin@users.noreply.github.com>
2020-12-22 04:10:37 +0000

    deploy: 00c246d0568e9fec21aa03f5b783e95bf517ea36

--- code.js
+++ code.js
@@ -1,1 +1,85 @@
-// Your Code Here!
+let game = document.getElementById("maze")
+
+//let currPosPlayer = game.setAttribute("player")
+//let playerTwo = game.setAttribute("player")
+//console.log(currPosPlayer)
+//console.log(playerTwo)
+//let row = document.createElement("row")
+//row.classList.add("row")
+//clickmode = "move"
+
+const map = [
+    "WWWWWWWWWWWWWWWWWWWWW",
+    "W   W     W     W W W",
+    "W W W WWW WWWWW W W W",
+    "W W W   W     W W   W",
+    "W WWWWWWW W WWW W W W",
+    "W         W     W W W",
+    "W WWW WWWWW WWWWW W W",
+    "W W   W   W W     W W",
+    "W WWWWW W W W WWW W F",
+    "S     W W W W W W WWW",
+    "WWWWW W W W W W W W W",
+    "W     W W W   W W W W",
+    "W WWWWWWW WWWWW W W W",
+    "W       W       W   W",
+    "WWWWWWWWWWWWWWWWWWWWW",
+];
+
+function movingInMaze() { //outer wall
+
+    for (let rows = 0; rows < map.length; rows++) {
+        for (let columns = 0; columns < map[rows].length; columns++) {
+            if (map[rows][columns] === "W") {
+                let wall = document.createElement("div")
+                wall.setAttribute("id", "wall")
+                game.append(wall)
+            }
+            if (map[rows][columns] === " ") {
+                let empty = document.createElement("div")
+                empty.setAttribute("id", "empty")
+                game.append(empty)
+
+
+                // default:
+            }
+            switch ((map[rows][columns])) {
+                case "S":
+                    let start = document.createElement("div")
+                    start.setAttribute("id", "start")
+                    game.append(start)
+
+                    break;
+
+
+            }
+        }
+        game.append(document.createElement("br"))
+    }
+}
+movingInMaze()
+
+//let row = document.createElement("row")
+//row.classList.add("row")
+//let currentposPlayer =
+
+// for (let i = 0; i < map[0].length; i++) {
+//     //console.log(game.row)
+
+
+
+
+//         //         //return row
+//         // } else {
+//         //     return;
+
+
+
+//         // }
+
+//     }
+
+// }
+
+//.addEventListerener("click", movingInMaze)
+//.addEventListener("click", movingInMaze)
--- readme.md
+++ readme.md
@@ -1,3 +1,68 @@
 # Building a Maze
+create global variables before 
+writing function 
+let current_Pos_player = player's current position in the maze
+let p2;
+player.setAtrribute("player) //playerId
+ 
+create a map function (map){
+Create outer wall for loop to create your
+row within the map array
+for (let i =0;i < map.length; i++) - travel map array)
+    //create div for row 
+    let row = document.createElement("div")
+    row.classList.add("row")
 
-Follow the instructions on my.kenzie.academy
+    for(let j = 0; j < map.length; j++)
+    cell div = maze a DIV
+    create a variable cell 
+    let cell = document.createElement("div")
+    cell.classList.add("cell");
+    cell.setAttribute("id", 'cell concat with j)
+
+switch(mapArray[j]) {
+    case "W"
+    cell.classList.add("wall")
+    break; 
+}
+
+}
+
+create second function for key event 
+function keyEvent(event) {
+
+    create switch cases similarly to 
+    the keyboard events activity 
+}
+
+final part
+create win condtion for either player 1 
+or player 2 to win (essentially
+whoever gets to the end of the maze is the winner)
+
+<!-- HTML: 
+
+1. cell make cell div
+2.  current_Pos_player_DIV to represent the player's current position in the maze
+
+
+CSS:
+
+1. cell_Div Makes each row of the maze a DIV using "display: flex;"
+2. current_Pos_player_DIV is absolutely-positioned
+
+JS:
+
+1. player DIV's current position on the screen, relative to the start element's current position on the screen and the size of your cells (in the case of 3A).
+
+2. change the absolute position of the player DIV.  -->
+
+
+Card: 
+Maze and player are displayed on the page, and the player starts on the start square.
+3 points
+Player can be moved through the maze by arrow keys.
+3 points
+Player cannot move onto walls or outside of the maze.
+3 points
+User is notified once the player reaches the finish square (no alert or console.log).
