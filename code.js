c3f290ab929b1ea897baff087281e8c9d912079e
Ladeeluc <laura.hatch.lh@gmail.com>
2020-12-22 21:11:40 -0500

    code need function for player, suck a little less

--- code.js
+++ code.js
@@ -1,12 +1,9 @@
 let game = document.getElementById("maze")
+let runTime = document.createElement("div")
+runTime.setAttribute('id', 'player')
+game.append(runTime)
 
-//let currPosPlayer = game.setAttribute("player")
-//let playerTwo = game.setAttribute("player")
-//console.log(currPosPlayer)
-//console.log(playerTwo)
-//let row = document.createElement("row")
-//row.classList.add("row")
-//clickmode = "move"
+let goTime;
 
 const map = [
     "WWWWWWWWWWWWWWWWWWWWW",
@@ -26,29 +23,45 @@ const map = [
     "WWWWWWWWWWWWWWWWWWWWW",
 ];
 
+
 function movingInMaze() { //outer wall
 
     for (let rows = 0; rows < map.length; rows++) {
         for (let columns = 0; columns < map[rows].length; columns++) {
+            goTime = { x: rows, y: columns } //movement
             if (map[rows][columns] === "W") {
                 let wall = document.createElement("div")
-                wall.setAttribute("id", "wall")
+                wall.dataset.x = rows
+                wall.dataset.y = columns //dataset set name of
+                wall.setAttribute("class", "wall")
                 game.append(wall)
             }
             if (map[rows][columns] === " ") {
                 let empty = document.createElement("div")
-                empty.setAttribute("id", "empty")
+                empty.dataset.x = rows
+                empty.dataset.y = columns
+                empty.setAttribute("class", "empty")
                 game.append(empty)
 
 
-                // default:
+
             }
             switch ((map[rows][columns])) {
                 case "S":
                     let start = document.createElement("div")
+                    start.dataset.x = rows
+                    start.dataset.y = columns
                     start.setAttribute("id", "start")
                     game.append(start)
+                    start.append(runTime)
+                    break;
 
+                case "F":
+                    let finish = document.createElement("div")
+                    finish.dataset.x = rows
+                    finish.dataset.y = columns
+                    finish.setAttribute("id", "finish")
+                    game.append(finish)
                     break;
 
 
@@ -59,27 +72,65 @@ function movingInMaze() { //outer wall
 }
 movingInMaze()
 
-//let row = document.createElement("row")
-//row.classList.add("row")
-//let currentposPlayer =
+function keyEvent(evt) {
+    switch (evt.code) { //regonizes what key was pressed
+        case "ArrowDown": //research what it is
+            console.log(goTime.x)
+            if (
+                map[goTime.x + 1][goTime.y] != 'W' && //change a = x, change b = y
+                goTime.x + 1 != map.length // needs to look at later??
+            ) {
+                goTime.x += 1;
+            }
+            break;
+        case "ArrowUp":
+            console.log("hi")
+            if (
+                map[goTime.x - 1][goTime.y] != 'W' &&
+                goTime.x - 1 != map.length
+            ) {
+                goTime.x -= 1;
+            }
 
-// for (let i = 0; i < map[0].length; i++) {
-//     //console.log(game.row)
+            break;
+        case "ArrowLeft":
+            if (
+                map[goTime.x][goTime.y - 1] != 'W' &&
+                goTime.y - 1 != map.length
+            ) {
+                goTime.y -= 1;
+            }
+            break;
+        case "ArrowRight":
+            if (
+                map[goTime.x][goTime.y + 1] != 'W' &&
+                goTime.y + 1 != map.length
+            ) {
+                goTime.y += 1;
+            }
+            break;
+
+    }
+
+}
+
+document.addEventListener("keydown", keyEvent)
 
 
+/*pull and adjust where goTime.a and goTime.b - 
+think of way to call for it move, from one square to
+ another then leave space,never let x and y got to !-1, dwn map length needs to b changed 
+ to equal hortizal length for right, arraow right needs to be every space*/
+
+/* Movement can be performed a couple different ways: 
+In the case of 3A, change the absolute position of the player DIV. 
+Or, in the case of 3B, append the player DIV to the next cell DIV. 
+(You could use "document.querySelector()", and the CSS selector for attributes to get 
+the next cell element by the indexes you set on it via data attributes.)
 
 
-//         //         //return row
-//         // } else {
-//         //     return;
+  
 
-
-
-//         // }
-
-//     }
-
-// }
-
-//.addEventListerener("click", movingInMaze)
-//.addEventListener("click", movingInMaze)
+/*Sanitize input
+//Put data into a data structure
+//Alter data into required format */
