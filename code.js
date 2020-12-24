0d69976d3d234b0b234f19c281b5e73dfb831150
kenzieadmin <kenzieadmin@users.noreply.github.com>
2020-12-24 00:37:38 +0000

    deploy: 14a93bc1a28941e45e89d3b2d7d8cedcd8094e52

--- code.js
+++ code.js
@@ -1,15 +1,22 @@
 let game = document.getElementById("maze")
 let runTime = document.createElement("div")
 runTime.setAttribute('id', 'player')
+runTime.classList.add("cell")
 game.append(runTime)
+
+//let endTime = {
+// x: 8,
+//y: 20
+//}
 
 let goTime = {
     x: 9,
     y: 0
 
 }
-let boxTop = 200; //
-let boxLeft = 25; //CSS
+
+let boxTop = 305; //
+let boxLeft = 8; //CSS
 
 const map = [
     "WWWWWWWWWWWWWWWWWWWWW",
@@ -34,12 +41,12 @@ function movingInMaze() { //outer wall
 
     for (let rows = 0; rows < map.length; rows++) {
         for (let columns = 0; columns < map[rows].length; columns++) {
-            //goTime = { x: rows, y: columns } //movement
             if (map[rows][columns] === "W") {
                 let wall = document.createElement("div")
                 wall.dataset.x = rows
                 wall.dataset.y = columns //dataset set name of
                 wall.setAttribute("class", "wall")
+                wall.classList.add("cell")
                 game.append(wall)
             }
             if (map[rows][columns] === " ") {
@@ -47,6 +54,7 @@ function movingInMaze() { //outer wall
                 empty.dataset.x = rows
                 empty.dataset.y = columns
                 empty.setAttribute("class", "empty")
+                empty.classList.add("cell")
                 game.append(empty)
 
 
@@ -58,9 +66,10 @@ function movingInMaze() { //outer wall
                     start.dataset.x = rows
                     start.dataset.y = columns
                     start.setAttribute("id", "start")
+                    start.classList.add("cell")
                     game.append(start)
                     start.append(runTime)
-                        //runTime.append(goTime)
+
 
                     break;
 
@@ -69,7 +78,9 @@ function movingInMaze() { //outer wall
                     finish.dataset.x = rows
                     finish.dataset.y = columns
                     finish.setAttribute("id", "finish")
+                    finish.classList.add("cell")
                     game.append(finish)
+
                     break;
 
 
@@ -81,46 +92,46 @@ function movingInMaze() { //outer wall
 movingInMaze()
 
 function keyEvent(evt) {
-    switch (evt.code) { //regonizes what key was pressed
-        case "ArrowDown": //research what it is
-            console.log(goTime.x)
+    switch (evt.code) {
+        case "ArrowDown":
             if (
-                map[goTime.x + 1][goTime.y] != 'W' && //change a = x, change b = y
+                map[goTime.x + 1][goTime.y] != 'W' &&
                 goTime.x + 1 != map.length // needs to look at later??
             ) {
+                console.log("  this is correct")
                 goTime.x += 1;
-                boxTop += 30
-
-
+                boxTop += 29
             }
             break;
         case "ArrowUp":
-            console.log("hi")
             if (
                 map[goTime.x - 1][goTime.y] != 'W' &&
-                goTime.x - 1 != map.length
+                goTime.x - 1 != -1
             ) {
+                console.log("   this is correct")
                 goTime.x -= 1;
-                boxTop -= 19
+                boxTop -= 29
             }
 
             break;
-        case "ArrowRight":
-            if (
-                map[goTime.x][goTime.y - 1] != 'W' &&
-                goTime.y - 1 != map.length
-            ) {
-                goTime.y -= 1;
-                boxLeft += 10
-            }
-            break;
         case "ArrowLeft":
             if (
-                map[goTime.x][goTime.y + 1] != 'W' &&
-                goTime.y + 1 != map.length
+                map[goTime.x][goTime.y - 1] != 'W' &&
+                goTime.y - 1 != -1
             ) {
+                goTime.y -= 1;
+                boxLeft -= 29
+            }
+            console.log("   this is correct")
+            break;
+        case "ArrowRight":
+            if (
+                map[goTime.x][goTime.y + 1] != 'W' &&
+                goTime.y + 1 != map[1].length
+            ) {
+                console.log("  this is correct")
                 goTime.y += 1;
-                boxLeft -= 20
+                boxLeft += 29
             }
             break;
 
@@ -134,7 +145,7 @@ document.addEventListener("keydown", keyEvent)
 
 
 
-/** get ble box to on red square-means in CSS add top = 200-40 px  and left = 5-10 px, 
+/** notes from Meghan(tutor) get ble box to on red square-means in CSS add top = 200-40 px  and left = 5-10 px, 
  * then two values, needs to make player ht and width same as wall 
  * then JS = var boxtop adn box left needs to match intial values of player top and player left in CSS
  * change incremention in switch statment usually 10 - 20 - last */
@@ -142,12 +153,15 @@ document.addEventListener("keydown", keyEvent)
 /* notes from Noel(tutor):pull and adjust where goTime.a and goTime.b - 
 think of way to call for it move, from one square to
  another then leave space,never let x and y got to !-1, dwn map length needs to b changed 
- to equal hortizal length for right, arraow right needs to be every space*/
+ to equal hortizal length for right, arraow right needs to be every space
+ 
+ 
+ create if statment for finish in arrow right if it hits finish write it's done, then display*/
 
 /**rubric for assignment
- * This criterion is linked to a Learning OutcomeMaze and player are displayed on the page, and the player starts on the start square.
+ * /This criterion is linked to a Learning OutcomeMaze and player are displayed on the page, and the player starts on the start square.
 3.0 pts
-This criterion is linked to a Learning OutcomePlayer can be moved through the maze by arrow keys.
+/This criterion is linked to a Learning OutcomePlayer can be moved through the maze by arrow keys.
 3.0 pts
 This criterion is linked to a Learning OutcomePlayer cannot move onto walls or outside of the maze.
 3.0 pts
